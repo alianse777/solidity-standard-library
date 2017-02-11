1a8d110c7f0704c11b4c94d3b7ca2d5420479388
alianse777 <alianse@keemail.me>
2017-02-11 19:43:28 +0600

    Update random.sol

--- random.sol
+++ random.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.0;
 
 /*
-#    Copyright (C) 2017  alainse777
+#    Copyright (C) 2017  alianse777
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
