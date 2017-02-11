bc6739e1909b90f36b00cd2ca5bb239b1d545c82
alianse777 <alianse@keemail.me>
2017-02-11 20:10:51 +0600

    Update multiprecision.sol

--- multiprecision.sol
+++ multiprecision.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.4.0;
+/*
+#    Copyright (C) 2017  alianse777
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
     Note:
@@ -7,6 +23,7 @@ pragma solidity ^0.4.0;
     double_t(1, 50) will be 1.5
     
     example:
+       import "multiprecision.sol";
        contract Test is Double
        {
            function test() internal
