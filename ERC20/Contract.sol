70ea880bef7444cc10ac0881b42c88adb21e590a
alianse777 <alianse77@protonmail.com>
2018-07-11 14:13:05 +0000

    Added ERC20

---
+++ ERC20/Contract.sol
@@ -0,0 +1,39 @@
+#pragma solidity ^0.4.0;
+
+import "./Interface.sol";
+
+contract ERC20 is ERC20Interface, Double {
+
+    function ERC20(
+        string name,
+        string symbol,
+        uint256 totalSupply,
+        uint8 decimals
+    ) 
+        public
+    {
+        require(totalSupply <= MAX_VALUE, "totalSupply too big!");
+        creator = msg.sender;
+        c_name = name;
+        c_symbol = symbol;
+        c_totalSupply = totalSupply;
+        balances[creator] = totalSupply;
+        dscale = decimals;
+    }
+
+    function name() public view returns (string name) {
+        name = c_name;
+    }
+    
+    function symbol() public view returns (string symbol) {
+        symbol = c_symbol;
+    }
+    
+    function totalSupply() public view returns (uint256 totalSupply) {
+        totalSupply = c_totalSupply;
+    }
+    
+    function decimals() public view returns (uint8 decimals) {
+        decimals = dscale;
+    }
+}
